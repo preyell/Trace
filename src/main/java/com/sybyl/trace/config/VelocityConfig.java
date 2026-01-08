89dcffe2b5fcb537ff202b1c2b328d6c99a1db1f
preyell <preethiraos@gmail.com>
2026-01-08 16:43:21 +0300

    Add VelocityConfig bean

---
+++ src/main/java/com/sybyl/trace/config/VelocityConfig.java
@@ -0,0 +1,23 @@
+// com.sybyl.trace.config.VelocityConfig.java
+package com.sybyl.trace.config;
+
+import java.util.Properties;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VelocityConfig {
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        Properties props = new Properties();
+        props.setProperty("resource.loader", "class");
+        props.setProperty("class.resource.loader.class",
+                "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        props.setProperty("input.encoding", "UTF-8");
+        props.setProperty("output.encoding", "UTF-8");
+        return new VelocityEngine(props);
+    }
+}
